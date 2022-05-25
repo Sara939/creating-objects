253ff7b630eb8bb4c8059d6032260635d26898b9
sara-likaount <saralikaunt@gmail.com>
2022-05-25 13:01:00 +0300

    class work

---
+++ main.js
@@ -0,0 +1,55 @@
+////////////////////////////////// class work:
+// var manager= {
+//     firstname: "sara",
+//     lastname: "likaount",
+//     age: 30,
+//     paychek: 10000
+// };
+
+// var emploey= {
+//     fullname: "sara likaount",
+//     dateofbirth: "8/1/92",
+//     email: "saralkaount@gmail.com",
+//     addras: "shtammper 3, netanya"
+// };
+
+// var fruit= {
+//     name: "smith",
+//     color: "green",
+//     kind: "apple"
+// };
+
+// document.write(fruit.name);
+// document.write("<br>");
+// document.write(fruit.color);
+// document.write("<br>");
+// document.write(fruit.kind);
+
+// var oueclass = {
+//     classnum: 1,
+//     studentnum: 4,
+//     studentsname: ["sara", "shlomo", "eldad", "yoni"]
+// };
+
+// // document.write(oueclass.studentsname);
+// // document.write(oueclass.studentsname[0]);
+// // document.write(oueclass.studentsname[1]);
+// // document.write(oueclass.studentsname[2]);
+// // document.write(oueclass.studentsname[3]);
+
+// for(i=0; i<oueclass.studentsname.length; i++){
+//     document.write(oueclass.studentsname[i]+"<br>");
+// }
+
+// var kinergarden= {
+//     name: "suzi",
+//     numofkids: 6,
+//     kidsnames: ["shlomo", "wldad", "yoni", "sara", "lior", "shimon"]
+// }
+
+// for(i=0; i<kinergarden["kidsnames"].length; i++){
+// document.write(kinergarden["kidsnames"][i]+"<br>");
+// }
+
+// var newkey= prompt("enter a key");
+// document.write(kinergarden[newkey]);
